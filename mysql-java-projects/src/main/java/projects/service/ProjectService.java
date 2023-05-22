3662d7edd6e1f5d14e897d611e91774ab4127d13
@yuri-58 <steventorres792@gmail.com>
2023-05-22 12:00:19 -0500

    Attempt to upload the original week 10 project

---
+++ mysql-java-projects/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,28 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId)
+			.orElseThrow(() -> new NoSuchElementException(
+						"Project with ID= " + projectId + " does not exist."));
+				
+	}
+	
+}
